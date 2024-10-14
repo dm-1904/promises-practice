446b34e7816abc5021e7da787828fa5c5d4507c3
damonryon <damon.ryon@gmail.com>
2024-10-13 19:00:56 -0700

    complete e5

--- exercises/e5.js
+++ exercises/e5.js
@@ -8,8 +8,9 @@
  *          export const Promise
  */
 
-export const attachTitle = () => {
+export const attachTitle = (arg) => {
   // Your code goes here...
+  return `DR. ${arg}`
 };
 
 /**
@@ -22,6 +23,10 @@ export const attachTitle = () => {
 
 export const getPromise = () => {
   // Your code goes here...
+  return new Promise((res) => {
+    res((console.log(attachTitle('MANHATTAN'))))
+  })
+
 };
 
 // === TEST YOURSELF ===
